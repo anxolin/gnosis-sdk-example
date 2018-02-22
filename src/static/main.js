69936f0b4aadb00bca7c1480d6cbdeb6c3080f92
Angel Rodriguez <anxolin@gmail.com>
2018-02-22 18:13:29 +0100

    Create categorical event

--- src/static/main.js
+++ src/static/main.js
@@ -1,6 +1,17 @@
 console.log('Loading Gnosis SDK Example...')
 const output = document.getElementById('output')
 
+
+Gnosis.create().then(async gnosis => {
+  console.log('Loaded gnosis', gnosis)
+
+  await mathExample(gnosis)
+  const ipfsHash = await publishEventExample(gnosis)
+  const oracle = await createOracleExample(ipfsHash, gnosis)
+  await createCategoricalEventExample(oracle, gnosis)
+})
+
+
 function log(title, message, logItems) {
   // console.log(title, message)
   const outputItem = document.createElement('div')
@@ -14,6 +25,7 @@ function log(title, message, logItems) {
 }
 
 async function mathExample(gnosis) {
+  console.log('mathExample: Init')
   const ONE = Math.pow(2, 64)
 
   const math = await gnosis.contracts.Math.deployed()
@@ -23,12 +35,14 @@ async function mathExample(gnosis) {
   log(
     'Math Example',
     'Loads Math contract and display the result of invoking an operation',
-    [ `<span class="code">Math.ln(3) = ${result}</span>`]
+    [`<span class="code">Math.ln(3) = ${result}</span>`]
   )
+  console.log('mathExample: Executed with result', result)
 }
 
 
-async function examplePublishEvent(gnosis) {
+async function publishEventExample(gnosis) {
+  console.log('publishEventDescription: Init')
   const logItems = []
 
   // Publish event
@@ -53,18 +67,51 @@ async function examplePublishEvent(gnosis) {
 
   // Load event
   const eventDescription = await gnosis.loadEventDescription(ipfsHash)
+
   const eventDescriptionJson = JSON.stringify(eventDescription)
   logItems.push(`
 <p>Load event. Event data:</p>
 <p class="code">${eventDescriptionJson}</p>`)
 
+  // Log result
   log('Publish Event', `Example on using <em>publishEventDescription</em> 
 and <em>loadEventDescription</em>`, logItems)
+
+  console.log('publishEventDescription: Published', ipfsHash)
+
+  return ipfsHash
 }
 
-Gnosis.create().then(gnosis => {
-  console.log('Loaded gnosis', gnosis)
+async function createOracleExample(ipfsHash, gnosis) {
+  console.log('createCentralizedOracle: Init')
+  const oracle = await gnosis.createCentralizedOracle(ipfsHash)
+  log('Create oracle', `Example on using <em>createCentralizedOracle</em>`, [
+    `Centralized Oracle created with address <span class="code">${oracle.address}</span>`
+  ])
 
-  mathExample(gnosis)
-  examplePublishEvent(gnosis)
-})
+  console.log('createCentralizedOracle: Created', oracle.address)
+  return oracle
+}
+
+
+async function createCategoricalEventExample(oracle, gnosis) {
+  console.log('createCategoricalEventExample: Init')
+  const event = await gnosis.createCategoricalEvent({
+    collateralToken: gnosis.etherToken,
+    oracle,
+    // Note the outcomeCount must match the length of the outcomes array published on IPFS
+    outcomeCount: 3
+  })
+  log('Create categorical event', `Example on using 
+<em>createCategoricalEvent</em> with a previously created oracle.`, [
+      `Categorical event created with address <span class="code">${event.address}</span>`,
+      `If you are in <strong>Rinkeby</strong> you can see the indexed event in:
+<a href="https://gnosisdb.rinkeby.gnosis.pm/api/events/${event.address}">https://gnosisdb.rinkeby.gnosis.pm/api/events/${event.address}</a>
+      `
+    ])
+
+  console.log('createCategoricalEventExample: created', event.address)
+  return event
+}
+
+
