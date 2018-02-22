0522a907f8e01da898c177645cb2f4556cad5a42
Angel Rodriguez <anxolin@gmail.com>
2018-02-22 16:34:28 +0100

    Display result in the HTML page

--- src/static/main.js
+++ src/static/main.js
@@ -1,4 +1,15 @@
 console.log('Loading Gnosis SDK Example...')
+const output = document.getElementById('output')
+
+function log (title, message) {
+  console.log(title, message)
+  const outputItem = document.createElement('div')
+  outputItem.innerHTML =`
+<h2>${title}</h2>
+<p>${message}</p>
+`
+  output.appendChild(outputItem)
+}
 
 async function mathExample(gnosis) {
   const ONE = Math.pow(2, 64)
@@ -6,7 +17,7 @@ async function mathExample(gnosis) {
   const math = await gnosis.contracts.Math.deployed()
   const lnResult = await math.ln(3 * ONE)
 
-  console.log('[mathExample] Math.ln(3) = %s', lnResult.valueOf() / ONE)
+  log('Math Example', 'Math.ln(3) = ' + lnResult.valueOf() / ONE)
 }
 
 Gnosis.create().then(gnosis => {
