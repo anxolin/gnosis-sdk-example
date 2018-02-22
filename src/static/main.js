69c8cf79a16805e4a747f1af4fe5e8ce0b6c7cd9
Angel Rodriguez <anxolin@gmail.com>
2018-02-22 16:23:15 +0100

    Add math contract usage example

--- src/static/main.js
+++ src/static/main.js
@@ -1,1 +1,16 @@
 console.log('Loading Gnosis SDK Example...')
+
+async function mathExample(gnosis) {
+  const ONE = Math.pow(2, 64)
+
+  const math = await gnosis.contracts.Math.deployed()
+  const lnResult = await math.ln(3 * ONE)
+
+  console.log('[mathExample] Math.ln(3) = %s', lnResult.valueOf() / ONE)
+}
+
+Gnosis.create().then(gnosis => {
+  console.log('Loaded gnosis', gnosis)
+
+  mathExample(gnosis)
+})
